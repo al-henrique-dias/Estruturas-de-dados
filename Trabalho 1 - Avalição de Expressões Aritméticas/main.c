0338a27bac5ef7311c4f357e945e9578f1f104cf
Henrique (º-º) <87720975+al-henrique-dias@users.noreply.github.com>
2025-05-15 10:11:41 -0400

    :sparkles: feature - sistema para encontrar simbolos e numeros na main por pilha (comentado)
    :sparkles: fearture - sistema para criar subarvores na main
    
    aviso: commit para salvar o trabalho comentado para possível acesso futuro

--- Trabalho 1 - Avalição de Expressões Aritméticas/main.c
+++ Trabalho 1 - Avalição de Expressões Aritméticas/main.c
@@ -7,13 +7,15 @@
 */
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "arvore.h"
 #include "pilha.h"
 
 int main(){
 
     int k = 0;
-    Arvore *arvore;
+    Arvore *arvore, *subarvore[10];
+    Pilha *simbolos = NULL, *numeros = NULL;
 
     printf("\nBem vindo! Quantas expressões avaliaremos agora?\n");
     scanf("%d", &k);
@@ -21,12 +23,47 @@ int main(){
 
     for(int i = 0; i < k; i++){
         scanf("%s", expressao[i]);
-        for(int j = 0; i < sizeof(expressao[i]); i++){
-            if(expressao[i][j] != '(' || expressao[i][j] != ')'){
-                
+        for(int j = 0; j < sizeof(expressao[i]); j++){//percorre a expressão i
+            int sub = 0;
+            if(expressao[i][j] == ')'){//quando um parênteses é encontrado na expressão, o programa começa a buscar pelos símbolos e números correspondentes
+                for(int k = j; k > 0; k--){
+                    if(ispunct(expressao[i][k]) && (expressao[i][k] != '(') && (expressao[i][k] != ')')){
+                        subarvore[sub] = arvore_cria(
+                            expressao[i][k],
+                            isalpha(expressao[i][k+1]) ?
+                                arvore_cria(expressao[i][k+1], arvore_criavazia(), arvore_criavazia()) :
+                                arvore_criavazia(),
+                            isalpha(expressao[i][k-1]) ?
+                                arvore_cria(expressao[i][k-1], arvore_criavazia(), arvore_criavazia()) :
+                                arvore_criavazia
+                        );
+                        sub++;
+                        // empilhar(expressao[i][k], &simbolos);
+                        printf("%c\n", expressao[i][k]);
+                        expressao[i][k] = 'v';//impede o reuso do simbolo encontrado
+                        // if(isalpha(expressao[i][k+1])){//PARA TESTES - isto está procurando por letras, alterar para isdigit(expressao[i][k+1])
+                        //     empilhar(expressao[i][k+1], &numeros);//número da esquerda
+                        //     printf("%c\n", expressao[i][k+1]);
+                        // }
+                        // if(isalpha(expressao[i][k-1])){//PARA TESTES - isto está procurando por letras, alterar para isdigit(expressao[i][k-1])
+                        //     empilhar(expressao[i][k-1], &numeros);//número da direita
+                        //     printf("%c\n", expressao[i][k-1]);
+                        // }
+                        k = 0;
+                    }
+                }
             }
         }
         printf("%s\n", expressao[i]);
+        while(simbolos != NULL){
+            printf("%c", simbolos->elemento);
+            desempilhar(&simbolos);
+        }
+        printf("\n");
+        while(numeros != NULL){
+            printf("%c", numeros->elemento);
+            desempilhar(&numeros);
+        }
     }
 
     return 0;

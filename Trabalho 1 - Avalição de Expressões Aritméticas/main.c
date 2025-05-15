552c7c5fe85c6addd75fa279bfc5382e96965fe3
Henrique (º-º) <87720975+al-henrique-dias@users.noreply.github.com>
2025-05-15 11:55:46 -0400

    :arrow-up: upgrade dependencies - o sistema agora armazena os numeros e simbolos em subárvores e identifica a raiz da árvore principal ao invés de guardar tudo em pilhas

--- Trabalho 1 - Avalição de Expressões Aritméticas/main.c
+++ Trabalho 1 - Avalição de Expressões Aritméticas/main.c
@@ -13,57 +13,53 @@
 
 int main(){
 
-    int k = 0;
-    Arvore *arvore, *subarvore[10];
-    Pilha *simbolos = NULL, *numeros = NULL;
+    int k = 0, sub = 0;
+    Arvore *subarvore[10];
 
     printf("\nBem vindo! Quantas expressões avaliaremos agora?\n");
     scanf("%d", &k);
     char expressao[k][50];
+    Arvore *arvore[k];
 
     for(int i = 0; i < k; i++){
+        printf("\nInsira a expressao %d:\n", i + 1);
         scanf("%s", expressao[i]);
-        for(int j = 0; j < sizeof(expressao[i]); j++){//percorre a expressão i
-            int sub = 0;
+        expressao[i][strlen(expressao[i])] = '\0';
+        sub = 0;
+        for(int j = 0; j < strlen(expressao[i]); j++){//percorre a expressão i
             if(expressao[i][j] == ')'){//quando um parênteses é encontrado na expressão, o programa começa a buscar pelos símbolos e números correspondentes
                 for(int k = j; k > 0; k--){
                     if(ispunct(expressao[i][k]) && (expressao[i][k] != '(') && (expressao[i][k] != ')')){
+                        if(j == strlen(expressao[i]) - 1){
+                            arvore[i] = arvore_cria(expressao[i][k], arvore_criavazia(), arvore_criavazia());
+                            arvore_imprime(arvore[i]);
+                            printf("raiz da arvore %d: %c\n", i, expressao[i][k]);
+                            k = 0;
+                            break;
+                        }
                         subarvore[sub] = arvore_cria(
-                            expressao[i][k],
-                            isalpha(expressao[i][k+1]) ?
-                                arvore_cria(expressao[i][k+1], arvore_criavazia(), arvore_criavazia()) :
-                                arvore_criavazia(),
-                            isalpha(expressao[i][k-1]) ?
+                            expressao[i][k],//raiz da subárvore
+                            isalpha(expressao[i][k-1]) ?//folha esquerda da subárvore
+                                //PARA TESTES - isto está procurando por letras, alterar para isdigit(expressao[i][k-1])
                                 arvore_cria(expressao[i][k-1], arvore_criavazia(), arvore_criavazia()) :
-                                arvore_criavazia
+                                arvore_criavazia(),
+                            isalpha(expressao[i][k+1]) ?//folha direita da subárvore
+                                //PARA TESTES - isto está procurando por letras, alterar para isdigit(expressao[i][k+1])
+                                arvore_cria(expressao[i][k+1], arvore_criavazia(), arvore_criavazia()) :
+                                arvore_criavazia()
                         );
+                        arvore_imprime(subarvore[sub]);
                         sub++;
-                        // empilhar(expressao[i][k], &simbolos);
-                        printf("%c\n", expressao[i][k]);
-                        expressao[i][k] = 'v';//impede o reuso do simbolo encontrado
-                        // if(isalpha(expressao[i][k+1])){//PARA TESTES - isto está procurando por letras, alterar para isdigit(expressao[i][k+1])
-                        //     empilhar(expressao[i][k+1], &numeros);//número da esquerda
-                        //     printf("%c\n", expressao[i][k+1]);
-                        // }
-                        // if(isalpha(expressao[i][k-1])){//PARA TESTES - isto está procurando por letras, alterar para isdigit(expressao[i][k-1])
-                        //     empilhar(expressao[i][k-1], &numeros);//número da direita
-                        //     printf("%c\n", expressao[i][k-1]);
-                        // }
+                        printf("raiz da subarvore %d: %c\n", sub - 1, expressao[i][k]);
+                        printf("esquerdo da subarvore %d: %c\n", sub - 1, expressao[i][k-1]);
+                        printf("direito da subarvore %d: %c\n", sub - 1,  expressao[i][k+1]);
+                        expressao[i][k] = 'v';//impede o reuso do simbolo encontrado nesta posição
                         k = 0;
                     }
                 }
             }
         }
         printf("%s\n", expressao[i]);
-        while(simbolos != NULL){
-            printf("%c", simbolos->elemento);
-            desempilhar(&simbolos);
-        }
-        printf("\n");
-        while(numeros != NULL){
-            printf("%c", numeros->elemento);
-            desempilhar(&numeros);
-        }
     }
 
     return 0;
--- Trabalho 1 - Avalição de Expressões Aritméticas/testes.c
+++ Trabalho 1 - Avalição de Expressões Aritméticas/testes.c
@@ -1,37 +1,17 @@
 #include <stdio.h>
-#include "arvore.h"
+#include <string.h>
 
 int main(){
 
-    Arvore* arvore = arvore_cria(
-        '+',
-        arvore_cria(
-            '*',
-            arvore_cria(
-                '+',
-                arvore_cria('3', arvore_criavazia(), arvore_criavazia()),
-                arvore_cria('6', arvore_criavazia(), arvore_criavazia())
-            ),
-            arvore_cria(
-                '-',
-                arvore_cria('4', arvore_criavazia(), arvore_criavazia()),
-                arvore_cria('1', arvore_criavazia(), arvore_criavazia())
-            )
-        ),
-        arvore_cria('5', arvore_criavazia(), arvore_criavazia())
-    );
+    char a[10];
 
-    arvore_imprime(arvore);
-    printf("\n\n");
-    pre_ordem(arvore);
-    printf("\n\n");
-    in_ordem(arvore);
-    printf("\n\n");
-    pos_ordem(arvore);
-    printf("\n\n");
-    printf("\n"+arvore_pertence(arvore, '3') != 0 ? "três está na árvore\n" : "três não está na árvore\n");
-    printf("\n"+arvore_pertence(arvore, '4') != 0 ? "quatro está na árvore\n" : "quatro não está na árvore\n");
-    printf("\nfim\n");
+    scanf("%s", a);
+    printf("\nstring %s\n", a);
+    printf("size of string %d\n", sizeof(a));
+    printf("string lenght %d\n", strlen(a));
+    a[strlen(a)] = '\0';
+    printf("string lenght %d\n", strlen(a));
+    printf("string %s\n", a);
 
     return 0;
 }

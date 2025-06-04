039f22aac5571f29162c766b5ff5789632faede6
Henrique (º-º) <87720975+al-henrique-dias@users.noreply.github.com>
2025-06-03 22:56:26 -0400

    :sparkles: Feature - Binary Search Tree has now 3 functions: create_node(), insert() and search()
    work started on remove() function for BST, but it's currently incomplete

--- Árvore Binária de Busca/binary-search-tree.c
+++ Árvore Binária de Busca/binary-search-tree.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include "binary-search-tree.h"
 
-Node* new_node(int item){
+Node * create_node(int item){
 
-    Node *new = (Node*)malloc(sizeof(Node));
+    Node *new_node = (Node*)malloc(sizeof(Node));
 
-    new->key = item;
-    new->left = NULL;
-    new->right = NULL;
+    new_node->key = item;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-    return new;
+    return new_node;
 
 }
 
-Node* insert(int new_key, Node* root){
+Node * insert(int new_key, Node* root){
 
-    Node *new = new_node(new_key);
+    Node *new_node = create_node(new_key);
 
     //if the tree is empty
     if(root == NULL)
-        return new;
+        return new_node;
 
     Node *parent = NULL, *current = root;
 
@@ -30,24 +30,68 @@ Node* insert(int new_key, Node* root){
             current = current->left;
         else if(new_key > current->key)
             current = current->right;
-        //if the key already exists
-        else
+        else //if the key already exists
             return NULL;
     }
 
     if(new_key < parent->key)
-        parent->left = new;
+        parent->left = new_node;
     else
-        parent->right = new;
+        parent->right = new_node;
 
-    return new;
-
-}
-
-void search(){
+    return new_node;
 
 }
 
-void remove(){
+Node * search(int key, Node* root){
+
+    Node *current = root;
+
+    while(current != NULL){
+        if(key < current->key)
+            current = current->left;
+        else if(key > current->key)
+            current = current->right;
+        else
+            break;
+    }
+
+    return current;
 
 }
+
+//work in progress
+void remove(int key, Node* root){
+
+    Node *node = search(key, root);
+
+    if(node == NULL){
+        printf("Key not found or tree is empty.");
+        return;
+    }
+    
+    if(node->right == NULL){
+        Node *aux = node->left;
+        node = aux->left;
+        free(aux);
+        return;
+    }
+
+    if(node->left == NULL){
+        Node *aux = node;
+        node = aux->right;
+        free(aux);
+        return;
+    }
+
+    Node *current = node->right;
+    int aux;
+
+    while(current->left != NULL)
+        current = current->left;
+    aux = node->key;
+    node->key = current->key;
+    current->key = aux;
+    
+
+}   
--- Árvore Binária de Busca/binary-search-tree.h
+++ Árvore Binária de Busca/binary-search-tree.h
@@ -7,16 +7,23 @@ struct binary_search_tree {
     Node* right;//caso algo dê errado, talvez seja a terminodelogia, tente: struct arvore* direito;
 };
 
-//Creates a new BST node with the parameter item as the node's key and NULL children
+//Creates a new BST node with a given item as the node's key and NULL children
 //@param item The new node's key
 //@return Pointer to the new node
-Node* new_node(int item);
+Node * create_node(int item);
 
 //Inserts a new node to a BST
 //@param new_key New node's key to be inserted
 //@param root Pointer to the tree's root
-//@return If the new key is not already in the tree, the function returns the pointer to the new node.
-//@return If it already exists, the function returns NULL
-Node* insert(int new_key, Node* root);
-void search();
-void remove();
+//@return If the new key is not already in the tree, the function returns a pointer to the new node.
+//@return If it already exists, the function returns NULL.
+Node * insert(int new_key, Node* root);
+
+//Searches for a given key in a BST
+//@param key Key being searched for
+//@param root Pointer to the tree's root
+//@return If the given key is found, the function returns a pointer to it's node.
+//@return If it is not found, the function returns NULL.
+Node * search(int key, Node* root);
+
+void remove(int key, Node* root);
